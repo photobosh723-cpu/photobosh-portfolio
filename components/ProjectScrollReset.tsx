e3b2c628cd9adf8f4c2077d29fc7a1c6681f047b
youssefgeorge2005-eng <photobosh723@gmail.com>
2026-08-31 23:18:58 +0300

    preserve home scroll position

--- components/ProjectScrollReset.tsx
+++ components/ProjectScrollReset.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
+
 
 export default function ProjectScrollReset() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+
 
   return null;
 }
